5723d0f24c0da89e436b201f0ddb499cb4e3e593
Swaraj Karmakar <swarajkarmakar5@gmail.com>
2024-01-21 04:20:36 +0530

    Updated

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+let currSong = new Audio();
 let getSongs = async function () {
   try {
     let response = await fetch("http://127.0.0.1:5500/songs");
     let data = await response.text();
-    // console.log(data);
 
     let parser = new DOMParser(); // Create a new instance of the DOMParser class
     let doc = parser.parseFromString(data, "text/html"); // Parse the HTML string
@@ -35,15 +35,70 @@ function getSongDetails(songTitle) {
 
   return { songName, artistName };
 }
+async function playMusic(songTitle, artist, pause = false) {
+  let src;
+  if (artist === "Unknown") {
+    src = "/songs/" + songTitle + ".mp3";
+  } else {
+    src = "/songs/" + songTitle + "%20-%20" + artist + ".mp3";
+  }
+
+  if (currSong.src !== src) {
+    currSong.src = src;
+    await currSong.load();
+  }
+
+  try {
+    if (!pause) {
+      await currSong.play();
+      play.src = "./img/pause1.svg";
+    }
+
+    document.querySelector(".song-info").innerHTML = `
+    <div class="info">
+      <div>Song - ${songTitle}</div>
+      <div>Artist - ${artist}</div>
+    </div>
+  `;
+    duration.innerHTML = "00:00";
+    totalDuration.innerHTML = "00:00";
+  } catch (error) {
+    console.error("Failed to play audio:", error);
+  }
+}
+
+async function pauseMusic() {
+  try {
+    await currSong.pause();
+    play.src = "./img/play.svg";
+  } catch (error) {
+    console.error("Failed to pause audio:", error);
+  }
+}
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  return `${String(minutes).padStart(2, "0")}:${String(
+    remainingSeconds
+  ).padStart(2, "0")}`;
+}
+
 async function main() {
   let songs = await getSongs();
-  console.log(songs);
+
+  const firstSong = Object.keys(songs)[1];
+  const [songName, artistName] = firstSong.split(' - ');
+
+  await playMusic(songName.trim(),artistName.trim(),true);
+
 
   const localSongs = document
     .querySelector(".local-songs")
     .getElementsByTagName("ul")[0];
 
   let isFirstIteration = true;
+  let currentlyPlayingLi = null;
 
   Object.entries(songs).forEach(([key, value]) => {
     if (!isFirstIteration) {
@@ -54,28 +109,94 @@ async function main() {
           <div>${songName}</div>
           <div>${artistName}</div>
         </div>
-        <img class="invert" src="./img/playnow.svg" alt="">
+        <img class="invert play-button" src="./img/playnow.svg" alt="">
       </li>`;
     } else {
       isFirstIteration = false;
     }
   });
-  //   const selectedSongKey = "Love Dose"; // Replace with the actual filename
-  //   const selectedSongUrl = songs[selectedSongKey];
 
-  //   if (selectedSongUrl) {
-  //     const audio = new Audio(selectedSongUrl);
-  //     // audio.play();
-  //   }
+  const allLiElements = localSongs.querySelectorAll("li");
+
+  function updatePlayPauseIcon(li, isPlaying) {
+    const playButton = li.getElementsByClassName("invert")[1];
+    playButton.src = isPlaying ? "./img/pause.svg" : "./img/playnow.svg";
+  }
+
+  allLiElements.forEach((li) => {
+    li.addEventListener("click", async () => {
+      const songTitle = li.querySelector(".info").firstElementChild.innerHTML;
+      const artTitle = li.querySelector(".info").lastElementChild.innerHTML;
+      console.log(songTitle, artTitle);
+
+      if (
+        currSong.src !== "/songs/" + songTitle + ".mp3" &&
+        currSong.src !== "/songs/" + songTitle + "%20-%20" + artTitle + ".mp3"
+      ) {
+        // Play the new song if it's different from the currently playing song
+        playMusic(songTitle.trim(), artTitle.trim());
+
+        // Update the play/pause icons
+        updatePlayPauseIcon(li, true);
+
+        // Pause the currently playing song if any
+        if (currentlyPlayingLi) {
+          const prevSongTitle =
+            currentlyPlayingLi.querySelector(".info").firstElementChild
+              .innerHTML;
+          const prevArtTitle =
+            currentlyPlayingLi.querySelector(".info").lastElementChild
+              .innerHTML;
+          pauseMusic(prevSongTitle.trim(), prevArtTitle.trim());
+          updatePlayPauseIcon(currentlyPlayingLi, false);
+        }
+
+        currentlyPlayingLi = li;
+      } else {
+        // Toggle play/pause for the current song
+        if (currSong.paused) {
+          currSong.play().catch((error) => {
+            console.error("Failed to play audio:", error);
+          });
+        } else {
+          currSong.pause();
+        }
+
+        // Update the play/pause icons
+        updatePlayPauseIcon(li, !currSong.paused);
+      }
+    });
+  });
+
+  play.addEventListener("click", async () => {
+    if (currSong.paused) {
+      currSong.play();
+      play.src = "./img/pause1.svg";
+    } else {
+      currSong.pause();
+      play.src = "./img/play.svg";
+    }
+  });
+
+  //Time update
+  currSong.addEventListener("timeupdate", () => {
+    // This event is triggered once
+    const totalDurationElement = document.getElementById("totalDuration");
+    totalDurationElement.textContent = formatTime(currSong.duration);
+  });
+
+  currSong.addEventListener("timeupdate", () => {
+    // This event is triggered continuously as the audio is playing
+    const durationElement = document.getElementById("duration");
+    durationElement.textContent = formatTime(currSong.currentTime);
+    const seekBar = document.querySelector(".seek");
+    const progressBar = seekBar.querySelector(".progress-bar");
+    const progress = (currSong.currentTime / currSong.duration) * 100;
+    progressBar.style.width = `${progress}%`;
+  });
+
+  //Add event listener to seekbar
+  
 }
 
-//For overscroll showing
-
-// document.getElementById('scrollable').addEventListener("mouseenter", function() {
-//   this.style.overflowY = 'scroll';
-// });
-
-// document.getElementById('scrollable').addEventListener("mouseleave", function() {
-//   this.style.overflowY = 'hidden';
-// });
 main();
